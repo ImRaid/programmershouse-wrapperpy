f0d3bb7ee3c12512bdcf26536c47b572d84bb067
AndriyZ4 <80629311+AndriyZ4@users.noreply.github.com>
2022-02-03 11:02:31 +0200

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -1,1 +1,10 @@
+import requests
+import json
 
+def joke():
+  url = "https://www.programmershouse-api.ga/joke"
+  response = requests.get(url)
+  data = response.text
+  parse_json = json.loads(data)
+  input = parse_json['ukraine']
+  return input
